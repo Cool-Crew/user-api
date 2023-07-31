d55c99fb4b2989de1c5b1eb4e0aa4918ae05fc2d
Olive251 <oliviataylor437@gmail.com>
2023-07-31 12:45:11 -0400

    quick fix to include getUsernames in ride-service

--- ride-service.js
+++ ride-service.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
 const bcrypt = require("bcryptjs");
+const { getUsernames } = require("./user-service");
 
 let mongoDBConnectionString = process.env.MONGO_URL;
 
